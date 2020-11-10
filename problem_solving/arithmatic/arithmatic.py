566d4365affacab9ad151cfbfb27bd709f801cdc
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-11-09 19:26:38 -0800

    division operators

---
+++ problem_solving/arithmatic/arithmatic.py
@@ -0,0 +1,18 @@
+def math( a, b):
+    addition = a + b
+    subtraction = a - b
+    multiplication = a * b
+    print(addition)
+    print(subtraction)
+    print(multiplication)
+
+def div( a,b):
+    
+
+
+if __name__ == '__main__':
+    # a = int(input())
+    # b = int(input())
+    math(a, b)
+
+
---
+++ tests/problem_solving/test_arithmatic.py
@@ -0,0 +1,8 @@
+import pytest
+from problem_solving.arithmatic.arithmatic import math
+
+def test_():
+    # actual = math(3, 5)
+    # expected = '8-215'
+    # assert actual == expected
+    pass
