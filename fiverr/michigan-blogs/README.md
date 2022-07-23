1ada6234108262950848f1bbdb1147725ad97746
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-07-22 22:28:16 -0700

    michigan psychological care.com blog articles

---
+++ fiverr/michigan-blogs/README.md
@@ -0,0 +1,25 @@
+# michigan psychological care
+- take in a word doc, convert to html, insert into php article card
+- add article to articles folder and link on main blog page
+- shift pagination 5 articles newest to oldest
+- related article navigation previous and next
+
+## july 2022
+- tuesdays (500)
+    - 05 - affirmations 
+    - 12 - simplicity
+    - 19 - wellness
+    - 26 - workaholics
+
+- thursday (1200)
+    - 28 - mental health
+
+## edits
+
+- blog/
+- blog/article.php
+    - date
+    - related articles
+- images/blog/2022/07
+
+- blog 01-02-03-04 +5 articles
---
+++ fiverr/michigan-blogs/php/7-3-national-compliment-your-mirrior-day-how-affirmations-can-help-you-build-your-confidence.php
@@ -0,0 +1,98 @@
+<?php 
+
+$img = '';
+$title = '7/3 National Compliment Your Mirror Day: How Affirmations Can Help You Build Your Confidence';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>7/3 National Compliment Your Mirror Day: How Affirmations Can Help You Build Your Confidence</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>July 05, 2022</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2022/07/7-3-national-compliment-your-mirrior-day-how-affirmations-can-help-you-build-your-confidence.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+
+                            <h1><strong>7/3 National Compliment Your Mirror Day: How Affirmations Can Help You Build Your Confidence</strong></h1>
+                            <p><span style="font-weight: 400;">July 3 is National Compliment Your Mirror Day. Now, before you roll your eyes too hard at the idea of complimenting your mirror, we are here to tell you the benefits could be powerful! Affirmations are so much more than some woohoo ritual that TikTok self-help guru is sharing. Science has actually found that the practice of complimenting and talking yourself up can have many benefits to your self-esteem, interpersonal relationships, and overall enjoyment of life!</span></p>
+                            <h2><strong>The Many Benefits of Affirmations</strong></h2>
+                            <p><span style="font-weight: 400;">The practice of affirmations has many benefits to offer those who use them. These benefits include:</span></p>
+                            <h3><strong>Reducing Stress Levels</strong></h3>
+                            <p><span style="font-weight: 400;">Chronic stress levels can be detrimental to the health of your body and your brain. Fortunately, many&nbsp;</span><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4814782/"><span style="font-weight: 400;">researchers</span></a><span style="font-weight: 400;">&nbsp;believe that positive thinking, such as the thoughts promoted by practicing affirmations can be linked to lower stress levels and higher resilience against the emotion.</span></p>
+                            <h3><strong>Motivating You</strong></h3>
+                            <p><span style="font-weight: 400;">It can be easy to feel discouraged when you do not believe that you can do something. Fortunately, self-affirmations may be just what you need to believe in yourself and find the motivation to push through difficult obstacles. Getting a job done requires motivation and, with a belief in yourself and a little confidence, affirmations can help you do just that!</span></p>
+                            <h3><strong>Mood Boosting</strong></h3>
+                            <p><a href="https://pubmed.ncbi.nlm.nih.gov/19091166/"><span style="font-weight: 400;">Studies</span></a><span style="font-weight: 400;">&nbsp;have found that self-assuring affirmations can be critical in the healing process from mental health conditions like depression and anxiety. This is because of the mood boosting benefits offered by the practice of reframing negative thoughts into more positive ones.</span></p>
+                            <h3><strong>Confidence Boosting</strong></h3>
+                            <p><span style="font-weight: 400;">Your overall confidence, self-esteem, and self-worth have a huge impact on your ability to live a high-quality life. Fortunately, affirmations can be a great tool in building your confidence levels, allowing you to be happier and healthier.</span></p>
+                            <h2><strong>Getting Started</strong></h2>
+                            <p><span style="font-weight: 400;">As you can see, affirmations can be a great mental health tool to use when you are feeling down and lacking self-confidence. However, it isn&rsquo;t always easy to get started. A few tips to begin using affirmations in your life include:</span></p>
+                            <ul>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Consider what you want to change about your thinking. Are you self-sabotaging through your negative self-thoughts? Do you struggle to stand up for yourself? Whatever it is you want to change about your thoughts and life, write down.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Write down some positive statements about the changes you wish to make in your life. For instance, if you want to learn to say &ldquo;no&rdquo; more, a great affirmation could be &ldquo;I deserve healthy boundaries and will set them to live a life I love.&rdquo; If you want to build the confidence for that job interview, a great affirmation could be. &ldquo;I am a great candidate for this job and deserve to work here.&rdquo;</span></li>
+                            </ul>
+                            <h1><strong>We Are Here for You</strong></h1>
+                            <p><span style="font-weight: 400;">Affirmations can be a great way to build the confidence you need to get through a wide variety of obstacles in your life. However, they are not magic, which means they may not work for everybody. If you have been putting in the work to build your confidence and just cannot seem to do it on your own, we are here for you. Our professional, caring therapists will work with you to figure out why you may be struggling with low self-esteem and ways you can build your confidence to feel better than before. If you would like to learn more about getting started,&nbsp;</span><a href="https://michiganpsychologicalcare.com/contact.php"><span style="font-weight: 400;">contact</span></a><span style="font-weight: 400;">&nbsp;us today!</span></p>
+                            <p><strong>References</strong></p>
+                            <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4814782/"><span style="font-weight: 400;">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4814782/</span></a></p>
+                            <p><strong>Keywords:</strong><span style="font-weight: 400;"> affirmations, confidence, self-esteem, mental health, therapist</span></p>
+                            <h1><br /><br /></h1>
+								<!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/7-12-national-simplicity-day-the-mental-health-benifits-of-simplifying-your-life.php">NATIONAL SIMPLICITY DAY: THE MENTAL HEALTH BENEFITS OF SIMPLIFYING YOUR LIFE ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
