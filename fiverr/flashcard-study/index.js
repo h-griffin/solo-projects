108d7353168d4633fc7b1f559db667e474db30b1
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-08-08 19:19:36 -0700

    random index, multi word phrase regex match quote, revert design, new imgs

--- fiverr/flashcard-study/index.js
+++ fiverr/flashcard-study/index.js
@@ -6,8 +6,6 @@ var script = document.getElementById("paste-script")
 
 document.querySelector("button").addEventListener("click", () => {start()});
 
-var currentIndex = 0;
-
 //add oninput listener to all text areas
 document.querySelectorAll("textarea").forEach( textarea => {
     textarea.addEventListener("change", () => {canStart()})
@@ -16,9 +14,15 @@ document.querySelectorAll("textarea").forEach( textarea => {
 // disable start button when all fields are filled
 function canStart() {
     console.log("== canStart() ==")
-    if (meaning.value && phrase.value && script.value){
+ 
+    meaningText = meaning.value.replace(/^\s+/, '').replace(/\s+$/, '');
+    phraseText = phrase.value.replace(/^\s+/, '').replace(/\s+$/, '');
+    scriptText = script.value.replace(/^\s+/, '').replace(/\s+$/, '');
+
+    if (meaningText !== '' && phraseText !== '' && scriptText !== ''){
         document.querySelector("button").disabled = false
     } 
+ 
 }
 
 // start cycle display
@@ -31,13 +35,30 @@ async function start() {
     var allArrs = []
     // split all textarea inputs by white space (single words)
     document.querySelectorAll("textarea").forEach( textarea => {
-        allArrs.push(textarea.value.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } ));
+        
+        try{
+            if(textarea.id == "paste-meaning" || textarea.id == 'paste-script'){
+                // regex whitespace
+                allArrs.push(textarea.value.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } ));
+            } else if (textarea.id == "paste-phrase") {
+                // regex replace single ' with double " ->  match double quote "
+                allArrs.push(textarea.value.replaceAll("'", '"').match(/".*?"/g).filter( function(e) { return e.trim().length > 0; } ));
+            }
+
+            // throw error if lists are different length
+            if(allArrs.every(arr => arr.length === allArrs[0].length)) throw "unequal input lengths";
+        } catch (error){
+            alert(error)
+        }
+
     })
 
+
+    var randomIndex = Math.floor(Math.random() * allArrs[0].length)
     //skip first delay
-    document.getElementById("meaning-text").innerHTML = allArrs[0][0];
-    document.getElementById("phrase-text").innerHTML = allArrs[1][0];
-    document.getElementById("script-text").innerHTML = allArrs[2][0];
+    document.getElementById("meaning-text").innerHTML = allArrs[0][randomIndex];
+    document.getElementById("phrase-text").innerHTML = allArrs[1][randomIndex];
+    document.getElementById("script-text").innerHTML = allArrs[2][randomIndex];
 
     // cycle all displays - 5 seconds
     var displayInterval = setInterval(() => {updateDisplay(displayInterval, allArrs)}, 5000);
@@ -46,16 +67,10 @@ async function start() {
 var updateDisplay = function(displayInterval ,allArrs){
     console.log("== setDisplays() ==")
 
-    if(currentIndex >= allArrs[0].length -1){
-        // clearInterval(displayInterval); // end loop
-
-        currentIndex = 0; // restart list
-    } else{
-        currentIndex++
-    }
-
-    document.getElementById("meaning-text").innerHTML = allArrs[0][currentIndex];
-    document.getElementById("phrase-text").innerHTML = allArrs[1][currentIndex];
-    document.getElementById("script-text").innerHTML = allArrs[2][currentIndex];
+    var randomIndex = Math.floor(Math.random() * allArrs[0].length)
+ 
+    document.getElementById("meaning-text").innerHTML = allArrs[0][randomIndex];
+    document.getElementById("phrase-text").innerHTML = allArrs[1][randomIndex];
+    document.getElementById("script-text").innerHTML = allArrs[2][randomIndex];
 
 };
