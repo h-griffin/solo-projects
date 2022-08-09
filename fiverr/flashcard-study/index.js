b14f87cab64331a86c30b31edfe5a49480122944
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-08-08 19:15:29 -0700

    flashcard study logic

--- fiverr/flashcard-study/index.js
+++ fiverr/flashcard-study/index.js
@@ -1,24 +1,19 @@
-
-
-
-
-
-// check input fields
-// read input fields
-// parse input fields
-// 3 arrays
-// cycle display at matching indexes
-
+ 
+// get reference to text areas
 var meaning = document.getElementById("paste-meaning")
 var phrase = document.getElementById("paste-phrase")
 var script = document.getElementById("paste-script")
 
 document.querySelector("button").addEventListener("click", () => {start()});
 
+var currentIndex = 0;
+
+//add oninput listener to all text areas
 document.querySelectorAll("textarea").forEach( textarea => {
-    textarea.addEventListener("input", () => {canStart()})
+    textarea.addEventListener("change", () => {canStart()})
 })
 
+// disable start button when all fields are filled
 function canStart() {
     console.log("== canStart() ==")
     if (meaning.value && phrase.value && script.value){
@@ -26,24 +21,41 @@ function canStart() {
     } 
 }
 
-
-
-function start() {
-    console.log("== start() ==")
-    console.log(meaning.value, phrase.value, script.value)
+// start cycle display
+async function start() {
+    // remove listener for start button activation
+    document.querySelectorAll("textarea").forEach( textarea => {
+        textarea.removeEventListener("change", () => {canStart()})
+    })
 
     var allArrs = []
-
+    // split all textarea inputs by white space (single words)
     document.querySelectorAll("textarea").forEach( textarea => {
         allArrs.push(textarea.value.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } ));
     })
 
-    console.log(allArrs)
+    //skip first delay
+    document.getElementById("meaning-text").innerHTML = allArrs[0][0];
+    document.getElementById("phrase-text").innerHTML = allArrs[1][0];
+    document.getElementById("script-text").innerHTML = allArrs[2][0];
 
-    for(var i = 0; i < allArrs.length; i++){
-        document.getElementById("display-meaning").innerHTML = allArrs[0][i].value;
-        document.getElementById("display-meaning").innerHTML = allArrs[1][i].value;
-        document.getElementById("display-meaning").innerHTML = allArrs[2][i].value;
-    }
-    
+    // cycle all displays - 5 seconds
+    var displayInterval = setInterval(() => {updateDisplay(displayInterval, allArrs)}, 5000);
 }
+
+var updateDisplay = function(displayInterval ,allArrs){
+    console.log("== setDisplays() ==")
+
+    if(currentIndex >= allArrs[0].length -1){
+        // clearInterval(displayInterval); // end loop
+
+        currentIndex = 0; // restart list
+    } else{
+        currentIndex++
+    }
+
+    document.getElementById("meaning-text").innerHTML = allArrs[0][currentIndex];
+    document.getElementById("phrase-text").innerHTML = allArrs[1][currentIndex];
+    document.getElementById("script-text").innerHTML = allArrs[2][currentIndex];
+
+};
