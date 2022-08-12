3a1f4d2d2c3c391696459420d886e492e355e39c
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-08-11 22:44:09 -0700

    michigan blogs 08 files

---
+++ fiverr/michigan-blogs/08/php/ways-you-can-help-a-loved-one-struggling-with-grief.php
@@ -0,0 +1,93 @@
+<?php 
+
+$img = '';
+$title = 'Ways You Can Help a Loved One Struggling with Grief';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Ways You Can Help a Loved One Struggling with Grief</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Aug 23, 2022</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2022/08/ways-you-can-help-a-loved-one-struggling-with-grief.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+
+                            <h1>Ways You Can Help a Loved One Struggling with Grief</h1>
+
+                            <p>Grief is a difficult, confusing emotion that everybody goes through at one point of their life or another. Watching somebody you care about experience grief can be difficult and painful. You may find yourself wishing they could find healing or searching for ways to support them. In this article, we will cover a few different ways you can give support to a loved one who is coping with grief. These options include:</p>
+                            <h2>Offer a Listening Ear</h2>
+                            <p>When someone is experiencing grief, it can be easy for them to feel as though they have to navigate their journey to healing alone. This is why one of the best ways you can offer them support is by simply listening to them. Let your loved one know that you are here to listen and offer support.&nbsp;</p>
+                            <p>As your loved one opens up to you, help them feel validated and supported in their emotions and experiences. Know that your friend is not likely looking for advice, a solution, or an explanation for the pain they are experiencing. They may just want to feel heard and loved. The validation and support you have to offer may be crucial to their healing process.</p>
+                            <h2>Be Compassionate and Understanding of Their Grieving Process</h2>
+                            <p>It is crucial to remember that no two grieving processes are the same. Because of this, there really is no right or wrong way of grieving, so long as the person is not putting themselves or others in danger. Every grieving process, however, will likely be associated with an emotional rollercoaster ride that leaves them appearing to feel find one moment and feeling overwhelmed with emotion the next. Allow your friend to follow their own grieving process and let them know you are here to support them for as long as they need it.&nbsp;</p>
+                            <h2>Offer to Help Your Friend with Little Daily Tasks</h2>
+                            <p>Grief can be exhausting, both mentally and physically. Because of this, your loved one may be struggling to get through their day, let alone perform all of the little daily tasks they are responsible for. Lightening their load by helping cook a meal, grab some groceries, do the dishes, or fold the laundry may be extremely helpful in <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8158955/">supporting</a> your friend.&nbsp;</p>
+                            <p>Your friend may not ask you directly to help them with these tasks if asked something like, &ldquo;is there anything I can help with?&rdquo; However, they may be more likely to accept the help if you offer help with specific tasks instead.</p>
+                            <h2>Encourage Them to Talk to a Professional if Needed</h2>
+                            <p>If your friend&rsquo;s grief has been impacting them for an extended period of time or is keeping them from being able to properly live their lives, they may need therapy for grief. Our <a href="https://michiganpsychologicalcare.com/contact.php">therapists</a> are here to help. Through therapy, your friend may receive the help they need to navigate difficult emotions, develop a better understanding of their experiences, and learn healthy coping mechanisms to encourage their path to healing.&nbsp;</p>
+                            <p><strong>References</strong></p>
+                            <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8158955/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8158955/</a></p>
+                            <p><strong>Keywords:</strong>  recognizing grief, therapy for grief, coping with grief, therapist</p>
+								<!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+                                <a href="/blog/national-relaxation-day.php"> ⇐ NATIONAL RELAXATION DAY </a>
+
+							</div>
+                            
+							<div class="col-12 col-md-auto text-center">
+                            <a href="/blog/national-grief-awareness-day.php"> NATIONAL GRIEF AWARENESS DAY ⇒</a>
+                                </div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
