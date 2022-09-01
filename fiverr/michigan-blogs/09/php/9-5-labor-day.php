af59b0f03845851e6a380382f4adc6ca661f2e80
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-09-01 15:45:29 -0700

    michigan blogs 09 files

---
+++ fiverr/michigan-blogs/09/php/9-5-labor-day.php
@@ -0,0 +1,103 @@
+<?php 
+
+$img = '';
+$title = '9/5 Labor Day: The Importance of a Healthy Work-Life Balance';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>9/5 Labor Day: The Importance of a Healthy Work-Life Balance</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Sept 6, 2022</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2022/09/9-5-labor-day.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+
+                            <h1>9/5 Labor Day: The Importance of a Healthy Work-Life Balance</h1>
+
+                            <p>What is life if you are constantly working and never allowing any time for living? While we all need our jobs in order to afford to live a high-quality life, allowing our work to bleed into our relationships, hobbies, and home life can have a devastating impact.</p>
+                            <p>Unfortunately, our society has fallen into the bad habit of glamorizing the &ldquo;All work and no play&rdquo; mindset. This mindset is likely the reason why the&nbsp;<a href="https://www.apa.org/monitor/2022/01/special-burnout-stress">American Psychological Association</a>&nbsp;found that a whopping 79% of employees who participated in the APA&rsquo;s 2021 Work and Well-being Survey had experienced work-related stress in the month prior to their participation. 3 out of 5 employees who participated in this survey reported finding themselves being negatively impacted by their work-related stress.</p>
+                            <h2>What is Burnout?</h2>
+                            <p>Burnout is an increasingly popular form of exhaustion that leads individuals to lose motivation, energy, and quality of their life. Burnout has often been thought to be related to chronic stress or a constant feeling of overwhelm. This form of exhaustion is most commonly associated with work. This means that employees will find themselves losing interest in their jobs, struggling to produce their usual quality of work, and may even feel reluctant to continue showing up to their job.</p>
+                            <h2>Signs of Burnout</h2>
+                            <p>Burnout isn&rsquo;t typically a sudden change to one&rsquo;s personality. It most commonly happens slowly but surely as a person finds themselves living to work, struggling to meet deadlines, and crumbling under constant stress and pressure. The signs of burnout include:</p>
+                            <ul>
+                            <li>Constantly feeling exhausted</li>
+                            <li>Feeling reluctant and resentful toward your job</li>
+                            <li>An inability to produce your usual performance at work</li>
+                            </ul>
+                            <h1>&nbsp;</h1>
+                            <p>Burnout may lead to more serious mental health concerns, such as depression or anxiety disorders. Taking care of your mental health by seeking help when you need it and working to build a healthy work-life balance can be crucial to living your happiest and healthiest life.</p>
+                            <h2>Tips for Creating a Healthy Work-Life Balance</h2>
+                            <p>A few tips for creating a healthier work-life balance include:</p>
+                            <ul>
+                            <li>Getting organized. Develop your time management skills, get your environment, work, and to-do list together, and learn to say &ldquo;no&rdquo; to prevent yourself from becoming overwhelmed.</li>
+                            <li>Setting boundaries. When you are at work, work hard. However, work to set boundaries that will prevent you from allowing your work to bleed into your home life. This could mean setting up a work phone that you are available from during working hours only, telling your boss you cannot work overtime this week, or saying &ldquo;no&rdquo; to extra projects that will leave you feeling stressed and overwhelmed.</li>
+                            <li>Prioritizing self-care. Taking care of your mental and physical health is crucial to producing high-quality work and living a high-quality life. Make time for your hobbies, loved ones, doctor&rsquo;s appointments, meals, and movement. Your mind and body will thank you.</li>
+                            </ul>
+                            <h2>We Are Here for You</h2>
+                            <p>If you are struggling with increased stress or burnout, we are here for you.&nbsp;<a href="https://michiganpsychologicalcare.com/contact.php">Contact</a>&nbsp;us today, and we will connect you with a therapist who will offer a supportive and listening ear, help you understand why you feel the way you feel, and find a therapy option that will help you feel like yourself again.</p>
+                            <p><strong>References</strong></p>
+                            <p><a href="https://www.apa.org/monitor/2022/01/special-burnout-stress">https://www.apa.org/monitor/2022/01/special-burnout-stress</a></p>
+                            <p>Keywords: burnout, stress, work-life balance, therapy</p>
+								<!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<!-- <a href="/blog/social-wellness-month-why-strong-relationships-are-so-important.php">⇐ SOCIAL WELLNESS MONTH WHY STRONG RELATIONSHIPS ARE SO IMPORTANT</a> -->
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/9-10-world-suicide-prevention-day.php">9/10 WORLD SUICIDE PREVENTION DAY ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
