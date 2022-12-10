dc2e9d6910afcb4ecc30c8c1719b532399f7a038
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-12-09 16:59:04 -0800

    december michigan files

---
+++ fiverr/php/michiganpsychologicalcare-blogs/12/php/grief-and-christmas-time.php
@@ -0,0 +1,119 @@
+<?php 
+
+$img = '';
+$title = 'Grief and Christmastime (Blue Christmas)';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Grief and Christmastime (Blue Christmas)</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Dec 12, 2022</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2022/12/grief-and-christmas-time.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+
+                                <h1><strong>Grief and Christmastime (Blue Christmas)</strong></h1>
+                                
+                                <p>Christmastime can be a magical time of year for some and a painful time for others. Many people miss lost loved ones more than ever as the Christmas lights go up and holiday festivities are in full swing. If you wonder why the holidays feel especially hard as you battle through grief or are searching for ways to cope with seasonal depression, this article is here to help you.</p>
+                                <h2><strong>Why Are the Holidays Harder for Those Grieving?</strong></h2>
+                                <p>The holidays tend to be a time of year when tradition calls for more family time and togetherness. Many celebrate the holidays by seeing loved ones they have not reunited with for a year, catching up with long-lost friends, and searching for ways to connect with one&rsquo;s community. When you face a loss, such as the death of a loved one or a divorce, the holidays can intensify the absence as the traditions and expectations seem to emphasize your loss.</p>
+                                <h2><strong>Am I Experiencing Grief?</strong></h2>
+                                <p>Grief is the mind&rsquo;s natural response to loss or trauma. While the emotions one deals with when grieving can be complicated and all over the place, there are a&nbsp;<a href="https://www.cdc.gov/mentalhealth/stress-coping/grief-loss/index.html">few common feelings</a>&nbsp;one may experience with their grief. These include:</p>
+                                <ul>
+                                    <li>Denial</li>
+                                    <li>Shock</li>
+                                    <li>Anger</li>
+                                    <li>Guilt</li>
+                                    <li>Mourning</li>
+                                    <li>Acceptance</li>
+                                </ul>
+                                <p>It was once believed that there were seven stages everyone went through when experiencing grief. However, the more we learn about the phenomenon, the more we realize that there is no one-size-fits-all description of the emotional roller coaster a person experiences after a significant loss.&nbsp;&nbsp;</p>
+                                <h2><strong>Coping with Grief During the Holidays</strong></h2>
+                                <p>If you are experiencing grief this holiday season, taking care of yourself is crucial to getting through these next few months. A few ways you can cope with your grief include:</p>
+                                <ul>
+                                    <li><strong>Allowing yourself to feel the emotions you are feeling</strong>. Grant yourself a little compassion and kindness as your emotions twist and turn every which way. Bottling up your emotions will do nothing but leave you feeling worse.</li>
+                                    <li><strong>Don&rsquo;t be afraid to say &ldquo;no.&rdquo;</strong>&nbsp;Participate only in activities that add to your joy, not your misery, this holiday season.&nbsp;</li>
+                                    <li><strong>Honor the memories of those no longer here</strong>. Hold a piece of your loved one close this holiday season by sharing stories of them, honoring their favorite traditions, or putting a piece of them on full display.</li>
+                                    <li><strong>Talk about it</strong>. Sometimes, all we need is a shoulder to lean on. Whether you decide to turn to a friend, loved one, or mental health counseling, talking about the complicated emotions you are experiencing this holiday season can feel like a significant weight has lifted from your shoulders.</li>
+                                </ul>
+                                <h2><strong>How to Tell if I Am Dealing with Seasonal Depression</strong></h2>
+                                <p>Seasonal depression, otherwise known as Seasonal Affective Disorder (SAD), is a form of depression that comes and goes with certain seasons. While SAD can impact some people in the spring and summer months, it most commonly occurs in the chill of fall and winter. A few&nbsp;<a href="https://www.nimh.nih.gov/health/publications/seasonal-affective-disorder">symptoms of SAD</a>&nbsp;can include:</p>
+                                <ul>
+                                    <li>Experiencing low all day every day during a certain season</li>
+                                    <li>Low energy</li>
+                                    <li>Changes to eating or sleeping patterns</li>
+                                    <li>Feeling agitated or irritable</li>
+                                    <li>Loss of interest in the things that once brought you joy</li>
+                                    <li>Social withdrawal</li>
+                                    <li>Foggy thoughts</li>
+                                    <li>Feeling worthless, guilty, or hopeless</li>
+                                    <li>Experiencing thoughts or ideations of death or suicide</li>
+                                </ul>
+                                <p>Any form of depression can quickly become serious. If you believe that you may be experiencing seasonal depression, reach out to your doctor or a&nbsp;<a href="http://michiganpsychologicalcare.com/contact.php">mental health professional</a>&nbsp;to help you get back on track to feeling happy and healthy again.</p>
+                                <p><strong>References:</strong></p>
+                                <p><a href="https://www.nimh.nih.gov/health/publications/seasonal-affective-disorder">https://www.nimh.nih.gov/health/publications/seasonal-affective-disorder</a></p>
+                                <p><a href="https://www.cdc.gov/mentalhealth/stress-coping/grief-loss/index.html">https://www.cdc.gov/mentalhealth/stress-coping/grief-loss/index.html</a></p>
+                                <p>keywords: coping with grief, grief at Christmas, seasonal depression, coping with seasonal depression, mental health counseling</p>                               
+                                                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/5-ways-you-can-volunteer-your-time.php">⇐ 5 WAYS YOU CAN VOLUNTEER YOUR TIME FOR YOUR MENTAL HEALTH</a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/the-mental-health-benefits-of-meditation.php">THE MENTAL HEALTH BENEFITS OF MEDITATION ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
