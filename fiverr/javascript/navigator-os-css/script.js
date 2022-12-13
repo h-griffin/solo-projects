9a7c4c9a263dc97be7445849145c7b3d7863cee2
Griffin <60614863+h-griffin@users.noreply.github.com>
2022-12-13 02:50:07 -0800

    manual weather api data display

---
+++ fiverr/javascript/navigator-os-css/script.js
@@ -0,0 +1,9 @@
+
+// add ios script if navigating from mac os device
+if( navigator.userAgent.search('Mac')!==-1 ){  document.head.innerHTML += `<link rel="stylesheet" type="text/css" href="assets/css/ios-layout.css" />` };
+
+// 
+if( navigator.userAgent.search('Mac')!==-1 ){  document.getElementById("ios-css").disabled = false };
+
+
+
---
+++ fiverr/javascript/weather-api/weather.js
@@ -0,0 +1,428 @@
+
+
+// kingston
+function kingston( ) {
+
+    return {
+        title: "Kingston",
+        days: [
+            {
+                day: "Sun",
+                dayNum: "12/18",
+                forecast: "Scattered Flurries",
+                icon: "",
+                temp: "0",
+                feelsLike: "-6",
+                night: "-6",
+                pop: "40%",
+                wind: "31W",
+                windGust: "47",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "~1cm"
+                ,city: "Kingston"
+            }, 
+            {
+                day: "Mon",
+                dayNum: "12/19",
+                forecast: "Mainly Cloudy",
+                icon: "",
+                temp: "-2",
+                feelsLike: "-9",
+                night: "-6",
+                pop: "30%",
+                wind: "33W",
+                windGust: "50",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "~1cm" 
+                ,city: "Kingston"
+            }, 
+            {
+                day: "Tue",
+                dayNum: "12/13",
+                forecast: "Mainly Sunny",
+                icon: "",
+                temp: "-3",
+                feelsLike: "-8",
+                night: "-8",
+                pop: "20%",
+                wind: "15N",
+                windGust: "23",
+                hrsOfSun: "6h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Kingston"
+            }, 
+            {
+                day: "Wed",
+                dayNum: "12/14",
+                forecast: "Mainly Sunny",
+                icon: "",
+                temp: "-3",
+                feelsLike: "-8",
+                night: "-4",
+                pop: "10%",
+                wind: "19N",
+                windGust: "29",
+                hrsOfSun: "7h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Kingston"
+            }, 
+            {
+                day: "Thurs",
+                dayNum: "12/15",
+                forecast: "Cloudy with Sunny Breaks",
+                icon: "",
+                temp: "3",
+                feelsLike: "-1",
+                night: "1",
+                pop: "30%",
+                wind: "20E",
+                windGust: "30",
+                hrsOfSun: "1h",
+                rain: "1-3mm",
+                snow: "1-3cm" 
+                ,city: "Kingston"
+            }, 
+            {
+                day: "Fri",
+                dayNum: "12/16",
+                forecast: "Mixed Precipitation",
+                icon: "",
+                temp: "2",
+                feelsLike: "-2",
+                night: "0",
+                pop: "80%",
+                wind: "19E",
+                windGust: "29",
+                hrsOfSun: "0h",
+                rain: "5-10mm",
+                snow: "~1cm" 
+                ,city: "Kingston"
+            }, 
+            {
+                day: "Sat",
+                dayNum: "12/17",
+                forecast: "Light Snow",
+                icon: "",
+                temp: "1",
+                feelsLike: "-4",
+                night: "-2",
+                pop: "70%",
+                wind: "29SW",
+                windGust: "44",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "~1cm" 
+                ,city: "Kingston"
+            }, 
+        ]
+      }
+}
+
+function ottawa( ) {
+
+    return {
+        title: "Ottawa",
+        days: [
+            {
+                day: "Sun",
+                dayNum: "12/18",
+                forecast: "Scattered Flurries",
+                icon: "",
+                temp: "0",
+                feelsLike: "-5",
+                night: "-6",
+                pop: "60%",
+                wind: "21W",
+                windGust: "32",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "~1cm" 
+                ,city: "Ottawa"
+            }, 
+            {
+                day: "Mon",
+                dayNum: "12/19",
+                forecast: "A Few Flurries",
+                icon: "",
+                temp: "-2",
+                feelsLike: "-9",
+                night: "-6",
+                pop: "30%",
+                wind: "33W",
+                windGust: "50",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "~1cm" 
+                ,city: "Ottawa"
+            }, 
+            {
+                day: "Tue",
+                dayNum: "12/13",
+                forecast: "Sunny",
+                icon: "",
+                temp: "-3",
+                feelsLike: "-8",
+                night: "-10",
+                pop: "10%",
+                wind: "15NW",
+                windGust: "23",
+                hrsOfSun: "9h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Ottawa"
+            }, 
+            {
+                day: "Wed",
+                dayNum: "12/14",
+                forecast: "Sunny",
+                icon: "",
+                temp: "-1",
+                feelsLike: "-4",
+                night: "-6",
+                pop: "0%",
+                wind: "13NW",
+                windGust: "20",
+                hrsOfSun: "9h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Ottawa"
+            }, 
+            {
+                day: "Thurs",
+                dayNum: "12/15",
+                forecast: "Cloudy with Sunny Breaks",
+                icon: "",
+                temp: "2",
+                feelsLike: "-3",
+                night: "0",
+                pop: "30%",
+                wind: "21E",
+                windGust: "32",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "2-4cm" 
+                ,city: "Ottawa"
+            }, 
+            {
+                day: "Fri",
+                dayNum: "12/16",
+                forecast: "Snow",
+                icon: "",
+                temp: "1",
+                feelsLike: "-4",
+                night: "0",
+                pop: "70%",
+                wind: "23E",
+                windGust: "35",
+                hrsOfSun: "0h",
+                rain: "",
+                snow: "10-15cm" 
+                ,city: "Ottawa"
+            }, 
+            {
+                day: "Sat",
+                dayNum: "12/17",
+                forecast: "Snow",
+                icon: "",
+                temp: "1",
+                feelsLike: "-1",
+                night: "-3",
+                pop: "60%",
+                wind: "12E",
+                windGust: "18",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "2-4cm" 
+                ,city: "Ottawa"
+            }, 
+        ]
+      }
+}
+
+function toronto( ) {
+
+    return {
+        title: "Toronto",
+        days: [
+            {
+                day: "Sun",
+                dayNum: "12/18",
+                forecast: "Cloudy with Sunny Breaks",
+                icon: "",
+                temp: "0",
+                feelsLike: "-6",
+                night: "-6",
+                pop: "30%",
+                wind: "30W",
+                windGust: "45",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Toronto"
+            }, 
+            {
+                day: "Mon",
+                dayNum: "12/19",
+                forecast: "A Mix of Sun and Clouds",
+                icon: "",
+                temp: "-1",
+                feelsLike: "-2",
+                night: "-3",
+                pop: "30%",
+                wind: "15W",
+                windGust: "23",
+                hrsOfSun: "2h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Toronto"
+            }, 
+            {
+                day: "Tue",
+                dayNum: "12/13",
+                forecast: "Sunny",
+                icon: "",
+                temp: "0",
+                feelsLike: "-3",
+                night: "-7",
+                pop: "0%",
+                wind: "14N",
+                windGust: "21",
+                hrsOfSun: "9h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Toronto"
+            }, 
+            {
+                day: "Wed",
+                dayNum: "12/14",
+                forecast: "A Mix of Sun and Clouds",
+                icon: "",
+                temp: "0",
+                feelsLike: "-4",
+                night: "-2",
+                pop: "20%",
+                wind: "17E",
+                windGust: "26",
+                hrsOfSun: "3h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Toronto"
+            }, 
+            {
+                day: "Thurs",
+                dayNum: "12/15",
+                forecast: "Snow",
+                icon: "",
+                temp: "1",
+                feelsLike: "-6",
+                night: "1",
+                pop: "80%",
+                wind: "41E",
+                windGust: "62",
+                hrsOfSun: "0h",
+                rain: "~5mm",
+                snow: "~10cm" 
+                ,city: "Toronto"
+            }, 
+            {
+                day: "Fri",
+                dayNum: "12/16",
+                forecast: "Cloudy with Showers",
+                icon: "",
+                temp: "3",
+                feelsLike: "2",
+                night: "-1",
+                pop: "70%",
+                wind: "21SE",
+                windGust: "32",
+                hrsOfSun: "1h",
+                rain: "~1mm",
+                snow: "<1cm" 
+                ,city: "Toronto"
+            }, 
+            {
+                day: "Sat",
+                dayNum: "12/17",
+                forecast: "Cloudy with Sunny Breaks",
+                icon: "",
+                temp: "1",
+                feelsLike: "-3",
+                night: "-4",
+                pop: "30%",
+                wind: "20W",
+                windGust: "30",
+                hrsOfSun: "1h",
+                rain: "-",
+                snow: "-" 
+                ,city: "Toronto"
+            }, 
+        ]
+      }
+}
+
+// average weekly temperature for each location
+
+
+ 
+function warmestDay (city) {
+    return city.reduce(
+        (prev, current) => {
+            return prev.temp > current.temp ? prev : current
+        }
+    ); 
+} 
+
+function warmestLocation (givenDay, a, b, c) {
+
+    var allLocations = [a,b,c];
+
+    // get all sundays
+    let filteredData;
+    for (let i = 0; i < allLocations.length; i++) {
+        filteredData = allLocations[i].days.filter(({ day }) => day == givenDay)
+    }
+
+    // get hottest sunday 
+    var result =  filteredData.reduce(
+        (prev, current) => {
+            return prev.temp > current.temp ? prev : current
+        }
+    ); 
+
+    return result.city;
+} 
+
+function avgTemp (city) {
+    length = 0,
+    total  = 0;
+
+    for (let i = 0; i < city.days.length; i++) {
+        total += Number(city.days[i].temp);
+        length++;
+    }
+    
+    return (total / length).toFixed(2)
+
+} 
+
+
+
+function sunnyDays (city) {
+
+    return city.filter(({ forecast }) => forecast.includes("Sunny"))
+} 
+
+ 
+module.exports = {
+    "kingston" : kingston,
+    "ottawa" : ottawa,
+    "toronto" : toronto,
+    "warmestDay" : warmestDay,
+    "warmestLocation" : warmestLocation,
+    "avgTemp" : avgTemp,
+    "sunnyDays" : sunnyDays,
+}
