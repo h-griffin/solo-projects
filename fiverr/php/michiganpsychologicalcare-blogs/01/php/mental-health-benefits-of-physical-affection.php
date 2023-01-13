4f9e97411465b247c8a4f1588ce00916f5e7969f
Griffin <60614863+h-griffin@users.noreply.github.com>
2023-01-12 19:13:02 -0800

    michigan january articles

---
+++ fiverr/php/michiganpsychologicalcare-blogs/01/php/mental-health-benefits-of-physical-affection.php
@@ -0,0 +1,105 @@
+<?php 
+
+$img = '';
+$title = 'The Mental Health Benefits of Physical Affection- National Hugging Day (January 21)';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>The Mental Health Benefits of Physical Affection- National Hugging Day (January 21)</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Jan 10, 2023</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2023/01/mental-health-benefits-of-physical-affection.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+
+                                <h1><strong>The Mental Health Benefits of Physical Affection- National Hugging Day (January 21)</strong></h1>
+                                
+                                <p>January 21 is National Hugging Day. You may find yourself wondering what hugging has to do with mental health. Let me tell you this: it has a lot to do with it. Have you ever found yourself feeling safer, less stressed, or happier as you feel the touch of a loved one?</p>
+                                <p>Perhaps you have laid in your partner&rsquo;s arms after a stressful day and found the tension seeping from your body. Or maybe you have found yourself feeling down in the dumps just to have your mood lifted by your toddler&rsquo;s hug. Or, maybe the feel of your mother&rsquo;s hand in yours granted you the boost of confidence you needed to ace that important presentation. No matter the situation, there really is no denying just how beneficial physical touch can be to our overall sense of wellbeing.</p>
+                                <h2><strong>Why is Physical Touch So Impactful?</strong></h2>
+                                <p>No matter how introverted you may claim to be, all of us are social creatures. We have to be, as it has played a significant part in the survival of the human race throughout all these years. Our ancestors needed to be a part of a community in order to survive and thrive. The larger the community, the safer the group was.</p>
+                                <p>Sure, the need for community may not be nearly as significant as it once was, but there really is no denying the emotional security that comes with feeling as though you belong to one. We are built to need each other, which is why our brains have developed a reward system to encourage us to build relationships and communities of our own.</p>
+                                <h2><strong>The Mental Health Benefits of Physical Affection</strong></h2>
+                                <h3><strong>It Can Be a Mood Booster</strong></h3>
+                                <p>Physical touch releases a number of the brain&rsquo;s happy hormones, including oxytocin (the bonding hormone), serotonin (the happy hormone), and dopamine (the pleasure hormone).</p>
+                                <h3><strong>It Can Relieve Stress</strong></h3>
+                                <p>Hugs and other forms of physical touch have been shown to reduce the brain&rsquo;s stress hormones, like cortisol.</p>
+                                <h3><strong>It Can Benefit Your Physical Health, too.</strong></h3>
+                                <p>The more our physical health thrives, the better our mental health will be. Physical affection has been associated with&nbsp;<a href="https://pubmed.ncbi.nlm.nih.gov/7595953/">lowered heart rates and blood pressure</a>, which can be beneficial to one&rsquo;s overall cardiovascular health. It has also been associated with a boosted immune system and shown to help with pain relief.</p>
+                                <h3><strong>It Can Lessen the Symptoms of Depression</strong></h3>
+                                <p>When we are lacking in physical touch, we can experience several negative feelings. Those who are lacking in physical touch often find themselves struggling with&nbsp;<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8424338/">symptoms of depression</a>. Those with depressive symptoms also tend to crave physical touch more.</p>
+                                <h3><strong>It Reduces Feelings of Loneliness</strong></h3>
+                                <p>Loneliness can leave one experiencing a wide range of negative emotions. Fortunately,&nbsp;<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7250541/">physical touch</a>&nbsp;has been shown to significantly reduce feelings of loneliness in many people.</p>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>Physical touch is incredibly beneficial to our mental health and overall wellbeing. However, it is not a miracle worker. If you have found that your mental health is struggling, our mental health counselors are here for you.&nbsp;<a href="http://michiganpsychologicalcare.com/contact.php">Contact</a>&nbsp;us today.</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://pubmed.ncbi.nlm.nih.gov/7595953/">https://pubmed.ncbi.nlm.nih.gov/7595953/</a>&nbsp;</p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8424338/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8424338/</a>&nbsp;</p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7250541/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7250541/</a></p>
+                                <p>Keywords: community, mental health, counseling, therapy, relationships</p> 
+                                
+                                
+                                
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/how-substance-use-impacts-your-brain.php">⇐ HOW SUBSTANCE USE IMPACTS YOUR BRAIN</a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/relationship-between-your-mental-health.php">THE RELATIONSHIP BETWEENN YOUR MENTAL HEALTH ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
