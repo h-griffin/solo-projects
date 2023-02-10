c3b8e6b28a26525bd3f2aa38ba90d69277cb879e
Griffin <60614863+h-griffin@users.noreply.github.com>
2023-02-09 22:41:13 -0800

    feb articles

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2023/02/php/What-is-Depression.php
@@ -0,0 +1,115 @@
+<?php 
+
+$img = '';
+$title = 'What is Depression?';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>What is Depression?</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Feb 28, 2023</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2023/02/What-is-Depression.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+
+                                <h1><strong>What is Depression?</strong></h1>
+                                
+                                <p>We have all had moments when we feel low and unmotivated. However, when those low moods or lack of motivation seem to be sticking around for too long, it may be a sign of something more serious. Depression is one of the most common mental illnesses in the world, with an estimated&nbsp;<a href="https://www.who.int/news-room/fact-sheets/detail/depression">5% of adults</a>&nbsp;experiencing the mental disorder worldwide. While depression is common, it is an incredibly serious disorder that, if left untreated, can have dire consequences. Fortunately, there are treatments available that have been found to be effective in treating the symptoms of the mental health disorder, regardless of the severity.</p>
+                                <h2><strong>What is Depression?</strong></h2>
+                                <p>Depression is a serious mood disorder that leaves a person consistently experiencing feelings of sadness, a lack of motivation, and a loss of interest in the things that once brought them joy. While it is normal to experience sadness from time to time, depression tends to last longer than two weeks and becomes more severe the longer it goes untreated.</p>
+                                <h3><strong>Depression Symptoms</strong></h3>
+                                <p>The symptoms of depression go so much deeper than experiencing low moods. While depression can look different for different people, a few common symptoms of the mood disorder include:</p>
+                                <ul>
+                                <li>A lack of energy</li>
+                                <li>Persistently feeling sad or hopeless</li>
+                                <li>Irritability</li>
+                                <li>Changes to eating or sleeping behaviors</li>
+                                <li>Loss of interest in the things that once brought joy</li>
+                                <li>Foggy thoughts, difficulty concentrating, and struggling to make decisions</li>
+                                <li>Negative thinking patterns</li>
+                                <li>Social withdrawal</li>
+                                <li>Low self-esteem</li>
+                                <li>feelings of worthlessness and guilt</li>
+                                <li>Thoughts or ideations of self-harm, death, or suicide</li>
+                                </ul>
+                                <h2><strong>Types of Depression</strong></h2>
+                                <p>Depression can take on many different shapes and forms. A few different types of depression include:</p>
+                                <ul>
+                                <li><strong>Clinical Depression (Major Depressive Disorder)</strong>: the most common form of depression where one experiences depressive symptoms that last longer than two weeks.</li>
+                                <li><strong>Persistent Depressive Disorder (PDD)</strong>: mild or moderate depressive symptoms that last at least two years.</li>
+                                <li><strong>Postpartum Depression (PPD) or Prenatal Depression</strong>: prenatal depression is a form of depression that occurs during pregnancy. Postpartum depression is a type of depression that develops in the weeks after having a baby.</li>
+                                <li><strong>Seasonal Depression (Seasonal Affective Disorder)</strong>: a form of depression that comes and goes with specific seasons. The most common form of seasonal depression is winter-onset seasonal affective disorder, which is a form of depression that occurs during the cooler months and fades during the spring and summer.</li>
+                                <li><strong>Disruptive Mood Dysregulation Disorder (DMDD)</strong>: a mood disorder impacting children and adolescents that leaves one experiencing lasting irritability, anger, and extreme loss of temper.</li>
+                                <li><strong>Premenstrual Dysphoric Disorder (PMDD):</strong>&nbsp;a more intense form of premenstrual syndrome (PMS) in which a woman in her child-bearing years experience extreme changes in mood that can impact their ability to enjoy life, beginning 7-10 days before her period begins.</li>
+                                <li><strong>Bipolar Disorder</strong>: a mental disorder in which one experiences episodes of depression along with mania or hypomania.</li>
+                                </ul>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>Struggling with depression can feel scary, lonely, and debilitating. If you believe you may be experiencing depression, know that you do not have to suffer alone. Our compassionate and caring counselors are here to help you figure out why you feel this way, discover healthy coping mechanisms for difficult moments, and find a treatment option that works best for you.&nbsp;<a href="http://michiganpsychologicalcare.com/contact.php">Contact</a>&nbsp;us today.</p>
+                                <p><strong>Resources</strong></p>
+                                <p><a href="https://www.who.int/news-room/fact-sheets/detail/depression">https://www.who.int/news-room/fact-sheets/detail/depression</a></p>
+                                <p>Keywords: depression, therapy for depression, depression symptoms, counselor</p>
+
+
+
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/Ways-to-Improve-Your-Romantic-Relationship.php">⇐ WAYS TO IMPROVE YOUR ROMANTIC RELATIONSHIP</a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/how-substance-use-impacts-your-brain.php.php"> HOW SUBSTANCE ABUSE IMPACTS YOUR BRAIN ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
