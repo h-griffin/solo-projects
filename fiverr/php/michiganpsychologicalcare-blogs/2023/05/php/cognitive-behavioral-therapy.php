6be8c74c5a9875318d97ef1d81ee1254208f7f60
Griffin <60614863+h-griffin@users.noreply.github.com>
2023-06-07 16:36:31 -0700

    michigan may files

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2023/05/php/cognitive-behavioral-therapy.php
@@ -0,0 +1,114 @@
+<?php 
+
+$img = '';
+$title = 'Cognitive Behavioral Therapy - What is it and How Do I Know if It Is Right for Me?';
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Cognitive Behavioral Therapy - What is it and How Do I Know if It Is Right for Me?</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>May 09, 2023</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2023/05/cognitive-behavioral-therapy.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>Cognitive Behavioral Therapy - What is it and How Do I Know if It Is Right for Me?</strong></h1>
+                                
+                                <p>Sometimes, it can seem as though your options for therapy are endless. How can a person even begin to make the right decision for their mental health? In your quest to find the best form of therapy for you, the chances are high that you have run into cognitive behavioral therapy (CBT).</p>
+                                <p>CBT is one of the most largely used forms of therapy and for good reason. So, is it right for you?</p>
+                                <h2><strong>What is Cognitive Behavioral Therapy?</strong></h2>
+                                <p>Cognitive behavioral therapy, otherwise known as CBT, is a form of psychotherapy that is used to treat many different <a href="https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral">mental health ailments and struggles</a>, including:</p>
+                                <ul>
+                                    <li>Anxiety disorder</li>
+                                    <li>Depression</li>
+                                    <li>Eating disorder</li>
+                                    <li>Substance dependency</li>
+                                    <li>Marital or family issues</li>
+                                    <li>Severe mental illnesses</li>
+                                </ul>
+                                <p>CBT is based on a few core principles, like the belief that our thinking patterns and behaviors can play a significant role in the psychological problems we may be facing. This belief leads to the thought that one can find healing from psychological issues by talking through their thoughts and feelings, developing healthy coping mechanisms, and changing harmful thinking patterns.</p>
+                                <p>In CBT, the therapist will work with their client to better understand why they are experiencing psychological issues. As the reasons become clearer, the therapist will work to help the client:</p>
+                                <ul>
+                                    <li>recognize and reevaluate harmful thoughts</li>
+                                    <li>build confidence</li>
+                                    <li>develop healthier coping skills</li>
+                                    <li>learn relaxation techniques to calm the mind and body</li>
+                                    <li>learn to navigate difficult life situations</li>
+                                </ul>
+                                <p>CBT focuses on teaching clients to develop the skills they need to care for their mental health themselves. The therapist will work with you during your session and assign homework to teach you to apply your new skills to the real world.</p>
+                                <h2><strong>How Can One Benefit from Cognitive Behavioral Therapy?</strong></h2>
+                                <p>CBT has been used to treat a wide variety of mental illnesses and struggles. While it may not be effective for everybody, it has been shown to be effective for a significant amount of people. One of the top qualifying factors to benefit from CBT is the client&rsquo;s willingness and ability to trust in, open up to, and participate with their therapist. Now, nobody expects a person to open up fully with a therapist they have just met. This is why the therapist and client must first focus on building a trusting relationship. Because of this, it is perfectly acceptable to try out a few different therapists before settling on one you can trust.</p>
+                                <p>CBT has been an incredibly effective tool for many different people. It is not always used alone, though. While some people have found improvements from using CBT alone, others have benefitted from CBT paired with other therapy techniques, and others have benefited from it when paired with medication.</p>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>Mental health plays a critical role in a person&rsquo;s overall well-being. When your mental health is struggling, other areas of your life will begin to struggle, as well. That is why it can be so important to find the method of therapy that works best for you.</p>
+                                <p>If you have been struggling with your mental health and are considering giving CBT a try for yourself, our kind and caring therapists are here for you.&nbsp;<a href="http://michiganpsychologicalcare.com/contact.php">Reach out today</a>.</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral">https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral</a></p>
+                                <p>Keywords: cognitive behavioral therapy, talk therapy, CBT, therapist</p>
+                                        
+
+
+
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/caring-for-your-mental-health-as-a-mother.php">⇐ CARING FOR YOUR MENTAL HEALTH AS A MOTHER </a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/ways-to-teach-your-child-to-care-for-their-own-mental-health.php"> WAYS TO TEACH YOUR CHILD TO CARE FOR THEIR OWN MENTAL HEALTH ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
