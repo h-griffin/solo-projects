fd384f2a6b5f1ca05f3e2eabdf24ff7101d6a3dd
Griffin <60614863+h-griffin@users.noreply.github.com>
2023-09-08 13:42:06 -0700

    07 michigan blogs

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2023/07/php/the-loneliness-epidemic.php
@@ -0,0 +1,108 @@
+<?php 
+
+$img = '';
+$title = "The Loneliness Epidemic: How Loneliness Can Impact Your Mental Health";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>The Loneliness Epidemic: How Loneliness Can Impact Your Mental Health</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>July 25, 2023</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2023/07/the-loneliness-epidemic.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>The Loneliness Epidemic: How Loneliness Can Impact Your Mental Health</strong></h1>
+                                
+                                <p>These days, people are feeling lonelier than ever. It&rsquo;s surprising, right? You would think that, with social media, advanced technology, and easier access to different communities, we would be feeling more connected than ever before. Unfortunately, that isn&rsquo;t the case. According to surveys conducted by the U.S. Census Bureau, Americans are spending less time with friends and&nbsp;<a href="https://www.pbs.org/newshour/show/why-americans-are-lonelier-and-its-effects-on-our-health">more time alone</a>&nbsp;now when compared to the beginning of the 2020 COVID-19 pandemic.</p>
+                                <h2><strong>Why Are We Feeling So Lonely?</strong></h2>
+                                <p>During the COVID-19 pandemic, it was obvious why we all felt so lonely. We were forced to isolate in order to protect our health and the health of our community. But now that the pandemic restrictions have eased up, you may be wondering why we all still feel so lonely.</p>
+                                <p>The truth is, there are millions of reasons why a person may feel lonely. These days, we tend to lean more on technology for connection rather than in-person connection. While technology is a great tool for staying connected when you can&rsquo;t meet up in person, it will never replace the value of face-to-face human interactions.</p>
+                                <p>Another potential reason so many of us are feeling lonely may be due to the fact that an increased number of people are working from home, learning from home, and even shopping from home. Many of our day-to-day opportunities for human interaction are being replaced by technology, allowing us to stay home more than ever before.</p>
+                                <p>These are just two huge reasons so many of us feel so lonely. However, there are many more. The more we socially isolate, the more we will struggle with feelings of loneliness, even if we are connecting with each other through the internet.</p>
+                                <h2><strong>The Impacts of Loneliness on Mental Health</strong></h2>
+                                <p>Loneliness can have many negative impacts on a person&rsquo;s mental health. Social connection plays a critical role in our health and overall wellbeing. When our social health is struggling, our quality of life will struggle, too. A few ways loneliness impacts&nbsp;<a href="https://www.apa.org/monitor/2019/05/ce-corner-isolation">mental health</a>&nbsp;include:</p>
+                                <ul>
+                                    <li>Depression</li>
+                                    <li>Anxiety</li>
+                                    <li>Sleep problems</li>
+                                    <li>Lowered confidence and self-esteem</li>
+                                    <li>Cognitive decline</li>
+                                </ul>
+                                <h2><strong>Ways to Cope with Loneliness</strong></h2>
+                                <h3><strong>Stay Connected</strong></h3>
+                                <p>Call your dad. Go to grandma&rsquo;s house. Hang out with your friends. It doesn&rsquo;t matter what ways you choose to stay connected with your loved ones and community. The most important thing is working to maintain face-to-face, deep connections with other people.</p>
+                                <h3><strong>Take a Break from Social Media</strong></h3>
+                                <p>Social media is great for keeping in touch with loved ones when you can&rsquo;t meet up in person. It can also be a great tool for staying connected with the world around you. Unfortunately, social media can also distract you from real-life connections and interaction, causing you to feel isolated and lonely.</p>
+                                <h3><strong>Talk to a Therapist</strong></h3>
+                                <p>Loneliness has a serious impact on your mental health, no matter how introverted you are. If you have found that your feelings of loneliness are causing your mental health to struggle, seeking therapy may be essential to feeling happier and healthier. If you are ready to seek the help of a counselor, we are here for you.&nbsp;<a href="http://michiganpsychologicalcare.com/contact.php">Contact</a>&nbsp;us today.</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://www.pbs.org/newshour/show/why-americans-are-lonelier-and-its-effects-on-our-health">https://www.pbs.org/newshour/show/why-americans-are-lonelier-and-its-effects-on-our-health</a></p>
+                                <p><a href="https://www.apa.org/monitor/2019/05/ce-corner-isolation">https://www.apa.org/monitor/2019/05/ce-corner-isolation</a></p>
+                                <p>Keywords: loneliness, social health, depression, therapy, counselor</p>
+                                
+ 
+
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/how-lying-impacts-mental-health.php">⇐ HOW LYING IMPACTS MENTAL HEALTH </a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/ways-to-improve-your-social-wellness.php"> WAYS TO IMPROVE YOUR SOCIAL WELLNESS ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
