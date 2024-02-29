1f6966ae33e8ef51624b68d0214273b2b5f12146
Griffin <60614863+h-griffin@users.noreply.github.com>
2024-02-29 01:19:32 -0800

    october michigan files

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2023/10/php/ways-to-check-in-with-your-mental-health.php
@@ -0,0 +1,110 @@
+<?php 
+
+$img = '';
+$title = "Ways to Check in with Your Mental Health";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Ways to Check in with Your Mental Health</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Oct 24, 2023</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2023/10/ways-to-check-in-with-your-mental-health.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>Ways to Check in with Your Mental Health</strong></h1>
+    
+                                <p>Your mental health plays a crucial role in your quality of life. Unfortunately, society has pressured many of us to push it off to the wayside for years. This has caused many to feel ashamed when facing mental health struggles, lack education in caring for their mental health, and lack the desire to care for their brain&rsquo;s overall well-being.</p>
+                                <p>This World Mental Health Day, let&rsquo;s collectively agree to remove that stigma surrounding mental health. Let&rsquo;s start by learning ways to check in with yourself to ensure your mental health and overall well-being is thriving- because you deserve to be your healthiest, happiest self.</p>
+                                <h2><strong>The Importance of Mental Health</strong></h2>
+                                <p>Your mental health is crucial for your overall well-being. It affects your:&nbsp;</p>
+                                <ul>
+                                    <li>physical health</li>
+                                    <li>social well-being</li>
+                                    <li>cognitive health</li>
+                                    <li>career</li>
+                                    <li>Perspective of the world</li>
+                                    <li>quality of life</li>
+                                </ul>
+                                <h2><strong>Ways to Check in With Your Mental Health</strong></h2>
+                                <p>One of the best ways to care for your mental health involves knowing when your mental health is struggling. Checking in with yourself can be a great way of keeping tabs on your mental well-being. To check in with your mental health, consider these five questions.&nbsp;</p>
+                                <h3><strong>How Are You Feeling?</strong></h3>
+                                <p>We&rsquo;ll start with the most obvious- how are you feeling? We ask each other this question so often that it often gets waved off with a simple, &ldquo;Oh, I&rsquo;m fine.&rdquo; When you ask yourself this question, though, I want you to genuinely consider how you feel.</p>
+                                <p>Have you noticed yourself feeling more stressed than usual? Has sadness become your new normal? Do your emotions feel out of control? Or are you feeling happy and healthy? Keep track of how you are really feeling. This can be one of the easiest ways to check in with your mental health.</p>
+                                <h3><strong>What Struggles and Worries Are Weighing on You?</strong></h3>
+                                <p>Consider what stressors are weighing on you. Is there something you can do about them?&nbsp;</p>
+                                <p>Consider what you can do to take these worries off your plate. Is there nothing you can do about them? Consider ways to let these go.&nbsp;</p>
+                                <h3><strong>Are You Caring for Yourself?</strong></h3>
+                                <p>Have you been feeling off but don&rsquo;t know why? Consider how you have been caring for yourself. Have you been setting and maintaining healthy boundaries? Have you been exercising and eating well? Have you been sleeping and giving yourself a chance to rest? Every single one of these self-care habits will impact your mental health.</p>
+                                <h3><strong>Are You Making Time for the Things You Enjoy?</strong></h3>
+                                <p>How often are you allowing yourself the time to have fun? It can be easy to push off fun and enjoyment without even realizing it, especially when life gets busy. Take the time to consider how often you are allowing yourself to do the things you enjoy.</p>
+                                <h3><strong>How is My Support System?</strong></h3>
+                                <p>Your support system plays a significant role in your well-being and resiliency. Take some time to consider who is in your corner. Who do you trust? Who would you turn to in your time of need? Who in your life is a great listener?&nbsp;</p>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>Are you not feeling like your happiest, healthiest self? You know that you deserve a high-quality life, and to live your best life, your mental health needs to thrive. If your mental health is not thriving, we are here for you. <a href="https://www.michiganpsychologicalcare.com/contact.php">Contact</a> us today, and our caring mental health professionals will work to help you begin feeling better.</p>
+                                <p>&nbsp;</p>
+                                <p>Keywords: mental health, mental illness, self-care, therapy, counselor, CBT</p>
+                                        
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/how-pain-impacts-your-mental-health.php">⇐ HOW PAIN IMPACTS YOUR MENTAL HEALTH </a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/when-to-seek-therapy-for-your-mental-health.php"> WHEN TO SEEK THERAPY FOR YOUR MENTAL HEALTH ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
