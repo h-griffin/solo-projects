e7ed563f63dc16d37549e2d69ef973e223514fa0
Griffin <60614863+h-griffin@users.noreply.github.com>
2024-02-29 01:21:11 -0800

    2024 file structure and january files

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2024/01/php/Mental-Health-Self-Care-Practices-You-Can-Start-Today.php
@@ -0,0 +1,120 @@
+<?php 
+
+$img = '';
+$title = "Mental Health Self-Care Practices You Can Start Today";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Mental Health Self-Care Practices You Can Start Today</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Jan 16, 2024</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2024/01/Mental-Health-Self-Care-Practices-You-Can-Start-Today.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>Mental Health Self-Care Practices You Can Start Today</strong></h1>
+                                
+                                <p>Happy self-love month! Yes, January was officially deemed self-love month, a month dedicated to caring and loving the person who will never leave your side- you! Many of us find it incredibly easy to love on the people around us, but end up struggling to find ways to love ourselves.</p>
+                                <p>Forgetting to love yourself can lead to a slew of struggles, like issues with:</p>
+                                <ul>
+                                    <li>Relationships</li>
+                                    <li>Confidence&nbsp;</li>
+                                    <li>Boundaries</li>
+                                    <li>Mental health</li>
+                                </ul>
+                                <p>This month, focus on loving YOU, because you deserve it. Don't know where to begin? Here are mental health self-care practices you can start today!</p>
+                                <h2><strong>Incorporating Mindfulness into Your Day</strong></h2>
+                                <p>Mindful practices like mindful breathing, journaling, and meditation force you to slow down, which allows your mind a desperately needed rest. This break granted to your mind can offer tons of <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4895748/#:~:text=In%20meditation%2C%20the%20feedback%20loop,life%20and%20improves%20overall%20health.">mental health benefits</a>, like:</p>
+                                <ul>
+                                    <li>Promoting clarity and focus</li>
+                                    <li>Decreased feelings of stress, anxiety, and depression</li>
+                                    <li>Improved cognitive functioning</li>
+                                    <li>Pain reduction</li>
+                                </ul>
+                                <h3><strong>Establishing Healthy Boundaries</strong></h3>
+                                <p>Yes, it can be difficult for many of us, but setting boundaries is crucial to self-care. Develop clear boundaries, communicate them clearly, learn to say no, and prioritize activities that bring peace, joy, and health.&nbsp;</p>
+                                <h3><strong>Move Your Body</strong></h3>
+                                <p>Not only does physical activity benefit your physical wellbeing, but also your mental health. Engaging in regular exercise releases mood-boosting hormones, like endorphins, and can leave you feeling happier, more confident, and healthier. This movement can include a brisk walk, yoga practice, or weightlifting. The options are endless.&nbsp;</p>
+                                <h3><strong>Positive Affirmations</strong></h3>
+                                <p>Many of us are our own worst critic. Despite what you may have heard, self-deprecation is not doing you any favors. Speaking positively about yourself, like you do when practicing positive affirmations, can build and strengthen positive beliefs about yourself, granting you the confidence you need to grow.&nbsp;</p>
+                                <h3><strong>Build a Healthy Support System</strong></h3>
+                                <p>Nurturing positive connections with friends, family, or support groups is vital for <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5633215/">mental health</a>. Surround yourself with people who uplift you. Share your struggles, goals, and rants with trusted individuals. When you need it, seek professional therapy. Building a strong support network, regardless of its appearance, enriches your sense of belonging and contributes to living a high-quality life.</p>
+                                <h3><strong>Have Hobbies That Bring You Joy</strong></h3>
+                                <p>Prioritize making time for activities that bring you joy, peace, and fulfillment. Doing so can provide a sense of accomplishment, contribute to a positive mindset, build self-confidence, and improve life satisfaction.&nbsp;</p>
+                                <h3><strong>Improve Your Sleep Hygiene</strong></h3>
+                                <p>Quality sleep is fundamental to your overall wellbeing, including your mental health. Establish a consistent sleep routine, create a comfortable sleep environment, and prioritize getting enough rest each night. If you are struggling to sleep, talk to your doctor. When you don't get enough sleep, it can negatively impact your mood, cognitive function, and overall mental resilience.</p>
+                                <h3><strong>Digital Detox</strong></h3>
+                                <p>Our devices and screens are full of an abundance of information, which can quickly become overwhelming. Constant exposure to screens and technology can contribute to feelings of stress and overwhelm. Schedule digital detoxes, allowing yourself time to unwind and be fully present.</p>
+                                <h3><strong>Seek Professional Support When Needed</strong></h3>
+                                <p>While many of these practices are great options for caring for your mental health, few things beat seeking the help of a mental health professional.&nbsp;</p>
+                                <p>We all experience mental health struggles. If you feel that your mental health is struggling, we are here for you. <a href="https://www.michiganpsychologicalcare.com/contact.php">Contact</a> us today, and our caring and skilled therapists will be here to help you.</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4895748/#:~:text=In%20meditation%2C%20the%20feedback%20loop,life%20and%20improves%20overall%20health">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4895748/#:~:text=In%20meditation%2C%20the%20feedback%20loop,life%20and%20improves%20overall%20health</a>&nbsp;</p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5633215/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5633215/</a></p>
+                                <p>Keywords: confidence, self-care, mental health, self-esteem, therapy</p>
+ 
+ 
+                                
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/How-Gratitude-Can-Be-Good-for-You.php">⇐ HOW GRATITUDE CAN BE GOOD FOR YOU</a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/What-is-Forgiveness-Therapy.php"> WHAT IS FORGIVENESS THERAPY ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
---
+++ fiverr/php/michiganpsychologicalcare-blogs/2024/01/php/What-is-Forgiveness-Therapy.php
@@ -0,0 +1,120 @@
+<?php 
+
+$img = '';
+$title = "What is Forgiveness Therapy?";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>What is Forgiveness Therapy?</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Jan 23, 2024</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2024/01/What-is-Forgiveness-Therapy.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>What is Forgiveness Therapy?</strong></h1>
+                               
+                                <p>Forgiveness therapy is a specialized form of therapeutic intervention that focuses on the powerful and healing act of forgiveness. You might be wondering what forgiveness has to do with mental health, but the gracious act has been shown to enhance mental health and well-being time after time.&nbsp;</p>
+                                <p>Grounded in therapeutic techniques, like cognitive behavioral therapy (CBT), forgiveness therapy explores the impact holding grudges has on your mental health and teaches you the transformative power of letting go.</p>
+                                <h2>What is Forgiveness Therapy?</h2>
+                                <p>Forgiveness therapy focuses on the concept of releasing resentment, anger, and bitterness toward yourself or another person. The treatment goes beyond ethical or moral obligations and delves into the psychological benefits that forgiveness has to offer. This therapeutic approach acknowledges the emotional weight carried by those who find themselves harboring grudges and aims to help clients begin the process of letting go.</p>
+                                <h2><strong>Cognitive Behavioral Therapy (CBT) in Forgiveness Therapy</strong></h2>
+                                <p>Forgiveness therapy often incorporates elements of Cognitive Behavioral Therapy (CBT), a widely recognized, praised, and effective therapeutic approach. CBT focuses on identifying negative thought patterns and behaviors and building habits and coping mechanisms to change them.&nbsp;</p>
+                                <p>In forgiveness therapy, CBT is used to explore and reshape the thought processes and toxic behaviors associated with holding onto grudges.</p>
+                                <p>Forgiveness therapy uses CBT to help clients recognize distorted and harmful thinking patterns related to past grievances. It then helps the client challenge these negative thoughts and replace them with more constructive, forgiving perspectives.&nbsp;</p>
+                                <p>By addressing negative thinking patterns, forgiveness therapy empowers individuals to break free from the cycle of resentment and bitterness.</p>
+                                <h2><strong>The Impact Holding a Grudge Has on Mental Health</strong></h2>
+                                <p>Harboring grudges and refusing to forgive can have profound consequences on mental health. Unresolved feelings of anger and resentment can lead to heightened stress levels, anxiety, and even depression.&nbsp;</p>
+                                <p>Chronic grudge-holding can create persistent emotional turmoil, hindering personal growth and interpersonal relationships.</p>
+                                <p>Forgiveness therapy seeks to break this toxic cycle by guiding individuals through a process of self-reflection and understanding. It encourages them to explore the roots of their anger and resentment, acknowledging the impact these emotions have on their mental well-being.</p>
+                                <h2><strong>The Process of Forgiveness</strong></h2>
+                                <p>Forgiveness therapy typically involves:</p>
+                                <ul>
+                                    <li>Acknowledging and expressing emotions.</li>
+                                    <li>Seeking a deeper understanding of why you are struggling to forgive.</li>
+                                    <li>Ultimately making a conscious decision to forgive.</li>
+                                </ul>
+                                <p>This process is typically facilitated with the guidance of a mental health professional.</p>
+                                <h2><strong>The Many Benefits of Forgiveness</strong></h2>
+                                <p>Forgiveness has many more benefits to offer than simply releasing anger. A few benefits of releasing grudges include:</p>
+                                <ul>
+                                    <li>Improved relationships</li>
+                                    <li>Reduced <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10120569/#:~:text=Empirical%20studies%20have%20shown%20that,outcomes%20of%20forgiveness%20is%20scarce.">anger, stress and anxiety</a></li>
+                                    <li>Less symptoms of depression</li>
+                                    <li>A stronger immune system</li>
+                                    <li>Boosted self-esteem</li>
+                                    <li>Improved hopefulness for the future</li>
+                                    <li>Lighter mood</li>
+                                </ul>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>The benefits of forgiveness extend beyond your own mental health, positively influencing daily life and interpersonal relationships. Individuals who undergo forgiveness therapy often report a sense of freedom, improved emotional well-being, and improved relationships.</p>
+                                <p>Sometimes, though, forgiveness doesn&rsquo;t always come easily. If you are struggling to forgive someone who has done you wrong and are facing the mental health consequences of it, we are here for you. <a href="https://www.michiganpsychologicalcare.com/contact.php">Contact</a> us today!</p>
+                                <p><strong>Resources</strong></p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10120569/#:~:text=Empirical%20studies%20have%20shown%20that,outcomes%20of%20forgiveness%20is%20scarce">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC10120569/#:~:text=Empirical%20studies%20have%20shown%20that,outcomes%20of%20forgiveness%20is%20scarce</a>&nbsp;</p>
+                                <p>Keywords: therapy, CBT, forgiveness, grudges, mental health </p>
+ 
+ 
+                                
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/Mental-Health-Self-Care-Practices-You-Can-Start-Today.php">⇐ MENTAL HEALTH SELF CARE PRACTICES YOU CAN START TODAY</a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/Why-Changing-Bad-Habits-is-So-Hard.php"> WHY CHANGING BAD HABITS IS SO HARD ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
