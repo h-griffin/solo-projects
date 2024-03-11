32b11d6b96316b2c2909f29b5b4b5f4e4d34848b
Griffin <60614863+h-griffin@users.noreply.github.com>
2024-03-10 22:13:27 -0700

    feb michigan files

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2024/02/php/how-career-counseling-can-benefit-you.php
@@ -0,0 +1,113 @@
+<?php 
+
+$img = '';
+$title = "How Career Counseling Can Benefit You";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>How Career Counseling Can Benefit You</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Feb 6, 2024</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2024/02/how-career-counseling-can-benefit-you.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>How Career Counseling Can Benefit You</strong></h1>
+     
+                                <p>When you pick a career, you want to choose carefully. This can quickly become an overwhelming process for many people. Knowing that you are making a decision that impacts you for the rest of your working life can feel like a lot of pressure. You will spend a significant amount of your life at work, so choosing something that you will thrive in is crucial.&nbsp;</p>
+                                <p>Fortunately, you don't have to make that decision alone. Career counseling is a form of counseling that focuses on helping clients find a profession that they are satisfied with. Career counselors can guide one through:</p>
+                                <ul>
+                                    <li>Career changes</li>
+                                    <li>Weighing career options</li>
+                                    <li>Choosing a degree path</li>
+                                    <li>Job searches</li>
+                                </ul>
+                                <h2><strong>The Ways Career Counseling Can Benefit You</strong></h2>
+                                <p>Career counselors are here to help guide you through planning professional life goals and decisions. As you navigate the many choices and obstacles associated with your career, career counselors can guide you along your journey.</p>
+                                <h3><strong>Set Short-Term Goals</strong></h3>
+                                <p>If you are in a toxic work environment, are considering a career change, or are trying to decide on the perfect job for you, setting short-term career goals is likely at the top of your priority list. Career counselors will act as a great guide for this.</p>
+                                <h3><strong>Understand Career Paths That Fit You</strong></h3>
+                                <p>In counseling, you can get to know yourself deeper than ever before. You will be doing a lot of inner exploration. The better you know yourself, the more confident you will be in your career path decision.&nbsp;</p>
+                                <h3><strong>Learn How to Build a Healthy Work-Life Balance</strong></h3>
+                                <p>Your work-life balance is crucial to your overall quality of life. In career counseling, you can learn how to set healthy boundaries with your job to build a work-life balance that works best for you.</p>
+                                <h3><strong>Develop Financial Wellness</strong></h3>
+                                <p>Your relationship with money is a deeply personal concern. In counseling, you can explore your relationship with finances and set goals to benefit your financial well-being.</p>
+                                <h3><strong>Access Helpful Resources and Information</strong></h3>
+                                <p>Career counselors may have access to resources and information that could be greatly beneficial to you, allowing you to make an even more informed decision about your career.</p>
+                                <h3><strong>Strengthen Communication and Interview Skills</strong></h3>
+                                <p>Communication is essential to your work life. Your communication skills will follow you through your interviews, peer relationships, and your workplace concerns and needs.&nbsp;</p>
+                                <h3><strong>Build Confidence</strong></h3>
+                                <p>Confidence is a necessary part of a healthy work life. It allows you to set healthy boundaries, find motivation in your work, and build healthy working relationships with others. Career counseling will help you build the confidence you need to get the job and thrive in it.</p>
+                                <h3><strong>Feel Supported Through Difficult Career Decisions</strong></h3>
+                                <p>Career counseling can have many <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3756630/">mental health benefits</a>, as well- thanks to the fact that clients feel supported through difficult decisions and moments in their careers. This can benefit a client's psychological well-being, allowing them a clear mind through their decision-making process.</p>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>Are you struggling to decide on your next professional steps in life? You are not alone. Whether a person is fresh out of high school or well into adulthood, many people need a guide when making career decisions.&nbsp;</p>
+                                <p>If you are in search of career counseling, we are here for you. Our professional and caring therapists will work with you as you navigate the difficult decisions in your professional life. <a href="https://www.michiganpsychologicalcare.com/services.php">Contact</a> us today.</p>
+                                <p><strong>Resources</strong></p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3756630/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3756630/</a></p>
+                                <p>Keywords: CBT, career counseling, quality of life, therapist</p>
+ 
+                                
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<!-- <a href="/blog/ways-to-support-grieving-friend.php">⇐ WAYS TO SUPPORT GRIEVING FRIEND </a> -->
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/how-talking-about-it-can-benefit-you.php"> HOW TALKING ABOUT IT CAN BENEFIT YOU ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
---
+++ fiverr/php/michiganpsychologicalcare-blogs/2024/02/php/ways-to-keep-your-relationship-healthy-and-happy.php
@@ -0,0 +1,161 @@
+<?php 
+
+$img = '';
+$title = "Ways to Keep Your Relationship Healthy and Happy";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Ways to Keep Your Relationship Healthy and Happy</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Feb 29, 2024</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2024/02/ways-to-keep-your-relationship-healthy-and-happy.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>Ways to Keep Your Relationship Healthy and Happy</strong></h1>
+
+                                <p>Falling in love is easy, but what about life after happily ever after? Maintaining a happy, healthy relationship is more important than you may believe.&nbsp;</p>
+                                <p>Your relationships play a crucial role in your overall quality of life, especially the relationship you have with a partner. However, building and maintaining a healthy and happy relationship can be challenging. Relationships will always have their fair share of ups and downs.</p>
+                                <p>However, with some effort, plenty of commitment, a little willingness to compromise, and these tips and tricks, you can keep your relationship healthy and happy.&nbsp;</p>
+                                <h2><strong>What Does it Mean to Have a Healthy Relationship?</strong></h2>
+                                <p>Every relationship is different. Our love stories differ significantly, which means each and every one of our relationships will be unique, too. Because of that, your version of a healthy relationship can look completely different from another's. The signs of a healthy relationship include:</p>
+                                <ul>
+                                    <li>Sharing common goals for the relationship you share.</li>
+                                    <li>Open and honest communication, even if that means disagreeing from time to time.</li>
+                                    <li>Trust and loyalty.</li>
+                                    <li>A meaningful connection.</li>
+                                    <li>Maintaining your own lives outside of the relationship, as well.</li>
+                                    <li>Plenty of effort and compromise on both ends of the relationship.</li>
+                                </ul>
+                                <h2><strong>Ways to Keep Your Relationship Happy and Healthy</strong></h2>
+                                <p>Falling in love with someone is easy. It's staying in love that can be the hard part. When you are falling in love with someone, everything is new and exciting. But once the butterflies in your stomach begin to settle and the honeymoon phase comes to an end, it will take some real effort from the both of you to keep your relationship healthy and happy.&nbsp;</p>
+                                <p>Here are some ways you can do just that:</p>
+                                <h3><strong>Make Time for Quality Time</strong></h3>
+                                <p>You will never stop getting to know your partner. As humans, we are growing and changing every single day.&nbsp;</p>
+                                <p>The more quality time you spend together, the closer the two of you will grow. At the beginning of your relationship, you and your partner probably couldn't get enough of each other. You soaked up every moment you could get.</p>
+                                <p>However, life has a way of pulling a couple apart, making it difficult to spend quality time together. Don't let life pull you apart from your lover. Prioritize making time for quality time by:</p>
+                                <ul>
+                                    <li>Scheduling quality time into your week.</li>
+                                    <li>Being adventurous and trying new things together.</li>
+                                    <li>Finding a common activity you both enjoy.</li>
+                                    <li>Making your quality time together fun and enjoyable.</li>
+                                </ul>
+                                <h3><strong>Make Time for Other Relationships</strong></h3>
+                                <p>It is important to remember that you and your partner are individuals outside of your relationship. This ensures that you are able to maintain your own happiness, health, and individuality, leaving plenty of room for your partner and you to focus on maintaining the health of your relationship.&nbsp;</p>
+                                <h3><strong>Express Your Gratitude</strong></h3>
+                                <p>A simple "thank you" can go a long way in a partnership. When we feel appreciated, we feel loved and seen. That is why thanking your partner, even for the little things, can be so impactful to your relationship. Studies have found that <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2692821/">gratitude is a key component of maintaining healthy and happy relationships</a>.&nbsp;</p>
+                                <h3><strong>Keep an Open Line of Communication</strong></h3>
+                                <p>Communication is key to a happy, healthy relationship. That includes both positive and negative communication. Learning healthy communication skills will prove to be crucial in maintaining not only your romantic relationship but your relationships with others, as well. None of us are mind readers, so possessing the skills to communicate our needs, complaints, and desires in an effective way is an important part of life. Improve your communication skills by:</p>
+                                <ul>
+                                    <li>Being direct in your speech and saying exactly what you mean</li>
+                                    <li>Really listening to what your partner is saying</li>
+                                    <li>Learning your partner's body language</li>
+                                </ul>
+                                <h3><strong>Take an Interest in Your Partner</strong></h3>
+                                <p>We all want to feel validated and supported in our relationships. However, you don't always need to make a grand gesture to make your partner feel this way. Little things- like listening to them talk about their career or acknowledging a pretty flower they point out- can ensure that your partner feels important to you. Take an interest in them and the things that bring them joy. They are the person you fell in love with, after all.</p>
+                                <h3><strong>Learn Healthy Ways to Manage Stress</strong></h3>
+                                <p>Life has a tendency to be stressful from time to time. There is no way around it. Fortunately, there are many healthy ways to cope with stress (that don't include taking it out on your partner). A few healthy coping tools you can use when feeling stressed include:</p>
+                                <ul>
+                                    <li>Engaging in relaxing activities like yoga, baking, and walking.</li>
+                                    <li>Working out.</li>
+                                    <li>Talking about it.</li>
+                                    <li>Making time for rest.</li>
+                                    <li>Social media and technology breaks.</li>
+                                    <li>Engaging in mindful activities like breathwork or meditation.</li>
+                                </ul>
+                                <h3><strong>Be Open to Compromise</strong></h3>
+                                <p>Fortunately, you and your partner are two different people with different likes, dislikes, and personalities. If we all matched our partners perfectly, that would end up quite boring. However, this fact means that there will be times when the two of you will have to come to a compromise. Being open to compromise will ensure that you and your partner are both able to have a voice in your relationship, allowing you to build a supportive and validating partnership.</p>
+                                <h3><strong>Don't Forget About Physical Intimacy</strong></h3>
+                                <p>Physical intimacy- including hugging, kissing, hand-holding, sex, and other forms of physical touch- is a key part of a relationship. We all crave touch and affection. We want to feel loved and understood. Affection boosts your brain's bonding hormone, otherwise known as oxytocin.</p>
+                                <p>Physical touch can decrease feelings of <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7250541/">loneliness</a> while boosting both partner's confidence, mental health, and mood. It can also reduce symptoms of stress and anxiety. When a couple is physically intimate with each other, they are building a happier, healthier relationship. Keep physical intimacy alive by:</p>
+                                <ul>
+                                    <li>Touching your partner more often in little ways like holding hands or touching their knee.</li>
+                                    <li>Openly communicating your sexual needs, vulnerabilities, and desires.</li>
+                                    <li>Making sex fun for both of you.</li>
+                                    <li>Fostering emotional intimacy.</li>
+                                    <li>Spending more quality time together.</li>
+                                </ul>
+                                <h3><strong>Handle Conflict Effectively</strong></h3>
+                                <p>Every couple argues and disagrees. Conflict is simply a part of a healthy relationship. You and your partner are different people, no matter how much time you spend together or how many things the two of you seem to have in common.&nbsp;</p>
+                                <p>However, in conflict, it is crucial to remember that you and your partner are a team. Handling conflict with the intention of meeting a solution as a team is crucial to maintaining a happy, healthy relationship. A few ways you can do this include:</p>
+                                <ul>
+                                    <li>Understanding why you are fighting.&nbsp;</li>
+                                    <li>Taking a break when things begin to get too tense.</li>
+                                    <li>Avoiding lashing out, escalating the fight, and using hurtful words.</li>
+                                    <li>Turn your complaints into requests.</li>
+                                    <li>Be honest and vulnerable with each other.</li>
+                                    <li>Use statements like "I feel" instead of statements like "You always."</li>
+                                    <li>Listen to your partner's perspective.</li>
+                                    <li>Don't shy away from apologizing to each other when things get out of hand.</li>
+                                </ul>
+                                <h3><strong>Seek Couple's Counseling When Needed</strong></h3>
+                                <p>Sometimes, our relationships need a little extra help. Life can come with many obstacles and issues that can be difficult to solve on your own. If you and your partner have been struggling to handle issues in your relationship, our experienced and compassionate counselors are here for you. <a href="https://www.michiganpsychologicalcare.com/contact.php">Contact </a>us today, and we will work to pair you with a counselor that is right for you.</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7250541/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7250541/</a></p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2692821/">https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2692821/</a></p>
+                                <p>Keywords: healthy relationships, mental health, communication, couple's counseling</p>
+                                                                
+                                
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/why-taking-care-of-your-relationships-is-so-important-for-your-mental-health.php">⇐ WHY TAKING CARE OF YOUR RELATIONSHIPS IS SO IMPORTANT FOR YOUR MENTAL HEALTH </a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/How-CBT-Can-Help-You-Reach-Your-Goals.php"> HOW CBT CAN HELP YOU REACH YOUR GOALS ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
