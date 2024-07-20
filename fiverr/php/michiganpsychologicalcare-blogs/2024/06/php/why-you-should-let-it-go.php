da9bd70e7a4f9392f6d26dd6b0d0396af0162460
Griffin <60614863+h-griffin@users.noreply.github.com>
2024-07-19 17:25:31 -0700

    michigan july files

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2024/06/php/why-you-should-let-it-go.php
@@ -0,0 +1,116 @@
+<?php 
+
+$img = '';
+$title = "Ways to Set Healthy Boundaries with Social Media";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Ways to Set Healthy Boundaries with Social Media</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>July 23, 2024</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2024/06/ways-to-set-healthy-boundaries-with-social-media.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>Ways to Set Healthy Boundaries with Social Media</strong></h1>
+
+                                <p>Social media can be an extremely helpful tool for connection, learning, and inspiration. However, it can have a dark side, too.&nbsp;</p>
+                                <p>Setting healthy boundaries with social media will allow you to reap the benefits without negatively impacting your mental health.&nbsp;</p>
+                                <p>Don&rsquo;t know where to begin? We&rsquo;re here to help. Try these tips and tricks for setting healthy boundaries with social media.&nbsp;</p>
+                                <h2><strong>How Social Media Impacts Mental Health</strong></h2>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/books/NBK594763/#:~:text=A%20majority%20of%20adolescents%20report,friends'%20lives%20(80%25).">Social media can have both a positive and negative impact</a> on mental health. On the positive side, it:</p>
+                                <h3><strong>Positive Impacts of Social Media:</strong></h3>
+                                <ol>
+                                    <li><strong>Connection and Support</strong>: Social media is a great platform for connecting with others, providing a sense of belonging and support, and alleviating feelings of isolation.</li>
+                                    <li><strong>Sharing &amp; Spreading Information</strong>: Social media platforms can be highly effective for sharing and spreading information.&nbsp;</li>
+                                    <li><strong>Education and Awareness</strong>: Social media can efficiently educate people on many topics. For instance, it can raise awareness of mental health conditions, promote self-care practices, and provide access to resources and professional help.</li>
+                                </ol>
+                                <h3><strong>Negative Impacts:</strong></h3>
+                                <ol>
+                                    <li><strong>Comparison</strong>: Constant exposure to curated and idealized highlight reels of other people&rsquo;s lifestyles can lead to negative self-comparison, which can quickly lead to feelings of inadequacy and envy, contributing to lower self-esteem.</li>
+                                    <li><strong>Cyberbullying</strong>: Social media can act as a platform for cyberbullying, harassment, and trolling. This can quickly begin to significantly impact mental health and well-being.</li>
+                                    <li><strong>Unrealistic Expectations</strong>: From filters to highlight reels, social media can set unrealistic expectations for impressionable minds.&nbsp;</li>
+                                    <li><strong>Addiction</strong>: Scrolling can be an addictive habit, with notifications and the fear of missing out (FOMO) reeling users ready for a break back in.&nbsp;</li>
+                                </ol>
+                                <p>Overall, the impact social media has on mental health can vary widely depending on a person&rsquo;s usage patterns, consumption, and insecurities and vulnerabilities.&nbsp;</p>
+                                <p>Mindful use and healthy boundaries are crucial for healthy social media use.&nbsp;</p>
+                                <h2><strong>How to Set Healthy Boundaries with Social Media</strong>&nbsp;</h2>
+                                <p>Setting healthy boundaries with social media can help mitigate its potentially negative impacts on mental health. A few ways to set these boundaries include:</p>
+                                <ol>
+                                    <li><strong>Setting Limits</strong>: Decide how much time you want to be spending on social media and figure out strategies for maintaining them.&nbsp;</li>
+                                    <li><strong>Establish Tech-Free Zones</strong>: Have a designated area or time zone in which social media is completely off-limits.&nbsp;</li>
+                                    <li><strong>Keep Your Feed Inspiring</strong>: Ensure that you are following and consuming content that leaves you feeling better. If you find yourself feeling down after scrolling, unfollow the accounts leading to those feelings.&nbsp;</li>
+                                    <li><strong>Turn Off Notifications</strong>: Notifications can act as a trigger for scrolling. Don&rsquo;t let that temptation in. Turn the notifications for your apps off.&nbsp;</li>
+                                    <li><strong>Set Boundaries on What You Share</strong>: Oversharing can lead to stress, which is why setting healthy boundaries around your posts can be essential when handling social media.&nbsp;</li>
+                                    <li><strong>Be Mindful with Your Use</strong>: Before logging on, take a moment to ask yourself why you are using social media.&nbsp;</li>
+                                    <li><strong>Schedule Social Media Breaks</strong>: Take regular breaks from social media, allowing yourself to recharge.&nbsp;</li>
+                                    <li><strong>Seek Support</strong>: Talk to friends, family, or a therapist if you find it challenging to maintain healthy boundaries with social media.&nbsp;</li>
+                                </ol>
+                                <h2><strong>We Are Here for You</strong></h2>
+                                <p>Are you struggling to set healthy boundaries with social media? If so, know that you do not have to face it alone. Our therapists are here for you. <a href="https://www.michiganpsychologicalcare.com/contact.php">Contact</a> us today.&nbsp;</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://www.ncbi.nlm.nih.gov/books/NBK594763/#:~:text=A%20majority%20of%20adolescents%20report,friends'%20lives%20(80%25)">https://www.ncbi.nlm.nih.gov/books/NBK594763/#:~:text=A%20majority%20of%20adolescents%20report,friends'%20lives%20(80%25)</a>.</p>
+                                <p>Keywords: phone addiction, social media, mental health, comparison</p>
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/ways-to-set-healthy-boundaries-with-social-media.php">⇐ WAYS TO SET HEALTHY BOUNDARIES WITH SOCIAL MEDIA </a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a  href="/blog/coping-with-a-panic-disorder.php"> COPING WITH A PANIC DISORDER ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+<?php include('../include/footer.php'); ?>
