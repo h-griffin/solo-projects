d6063050032191e220423cd4cebc717b856564a6
Griffin <60614863+h-griffin@users.noreply.github.com>
2026-03-17 00:09:56 -0700

    michigan blogs 02/2026

---
+++ fiverr/php/michiganpsychologicalcare-blogs/2026/02/php/how-to-recognize-and-reframe-catastrophic-thinking.php
@@ -0,0 +1,97 @@
+<?php 
+
+$img = '';
+$title = "How to Recognize and Reframe Catastrophic Thinking";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>How to Recognize and Reframe Catastrophic Thinking</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Feb 3, 2026</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2026/02/how-to-recognize-and-reframe-catastrophic-thinking.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>How to Recognize and Reframe Catastrophic Thinking</strong></h1>
+                                <p>Small mistakes are a part of everyday life. However, sometimes a small mistake has the power to derail a person&rsquo;s whole day if they focus on it. This is an example of catastrophic thinking. <a href="https://beckinstitute.org/blog/catastrophic-thinking-a-transdiagnostic-process-across-psychiatric-disorders/">Catastrophic thinking</a> is a cognitive distortion within Cognitive Behavioral Therapy (CBT). CBT is a therapy modality that teaches people how their emotions, thoughts, and behaviors are all connected. CBT teaches <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC8489050/">cognitive distortions</a>, which are common error patterns that people have in their thinking. People can use CBT tools and therapy to help challenge catastrophic thinking and live a happier, fuller life.</p>
+                                <h2><strong>What Is Catastrophic Thinking?</strong></h2>
+                                <p>Catastrophic thinking is defined as a person who usually assumes the worst possible outcome in any situation. These thoughts are usually automatic and difficult to change because they are a pattern. People who deal with catastrophic thinking typically will escalate quickly into extremes. Catastrophic thinking usually brings intense feelings of anxiety.</p>
+                                <h2><strong>Why the Brain Defaults to Catastrophic Thinking</strong></h2>
+                                <p>Our brains are wired to keep us safe. From an evolutionary perspective, our brains try to protect us from threats. However, sometimes our brains will detect something as a threat or danger that actually is not a threat. An example of this is our brains responding to a catastrophic thought. Our brains will overestimate the amount of danger possible and underestimate our ability to handle it. This only creates more catastrophic thinking and anxiety.</p>
+                                <h2><strong>Effects of Catastrophic Thinking</strong></h2>
+                                <p>Catastrophic thinking has emotional, behavioral, and cognitive effects. These effects can reinforce the thinking patterns over time. The emotional effects of this type of thinking are anxiety, panic, or hopelessness. As far as behavioral effects, people will tend to avoid or procrastinate things related to their catastrophic thinking. Cognitively, this type of thinking can cause difficulty focusing or making decisions.</p>
+                                <h2><strong>How CBT Helps Catastrophic Thinking</strong></h2>
+                                <p>Cognitive Behavioral Therapy (CBT) teaches several <a href="https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral">CBT tools</a> that can help create balanced, more accurate thinking patterns. CBT can help people reframe their thoughts by becoming aware of their thoughts, finding the cognitive distortions present, examining the evidence for the thought, and reframing a new, more accurate thought. This process is referred to as <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC10440210/">cognitive restructuring</a>. CBT also provides other tools, such as thought records and behavioral experiments, that can be helpful in combating catastrophic thinking.</p>
+                                <p>Catastrophic thinking is a common thought pattern that people experience at different points in their lives. This type of thinking doesn&rsquo;t mean that the person is doing anything wrong. The good news is that CBT has the power to help people reframe their catastrophic thinking. By using CBT tools and other <a href="https://www.nimh.nih.gov/health/topics/psychotherapies">therapy skills</a>, people can create more balanced thoughts and decrease their anxiety levels. Changing thinking patterns can be difficult, especially if they have been present for a long time.</p>
+                                <p>If you are reading this and feel like you or someone you love struggles with catastrophic thinking, help is available. <a href="https://michiganpsychologicalcare.com/contact.php">Reach out to us</a> today if you are ready to get started.&nbsp;</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://beckinstitute.org/blog/catastrophic-thinking-a-transdiagnostic-process-across-psychiatric-disorders/">https://beckinstitute.org/blog/catastrophic-thinking-a-transdiagnostic-process-across-psychiatric-disorders/</a>&nbsp;</p>
+                                <p><a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC8489050/">https://pmc.ncbi.nlm.nih.gov/articles/PMC8489050/</a>&nbsp;</p>
+                                <p><a href="https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral">https://www.apa.org/ptsd-guideline/patients-and-families/cognitive-behavioral</a>&nbsp;</p>
+                                <p>https://pmc.ncbi.nlm.nih.gov/articles/PMC10440210/</p>
+                                <p><a href="https://www.nimh.nih.gov/health/topics/psychotherapies">https://www.nimh.nih.gov/health/topics/psychotherapies</a>&nbsp;</p>
+                                <p><strong>Keywords</strong>: CBT tools, cognitive distortions, therapy skills, cognitive restructuring</p>
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<!-- <a href="/blog/the-science-behind-antidepressants-how-they-actually-work.php">⇐ the science behind antidepressants how they actually work </a> -->
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a  href="/blog/mindfulness-for-anxiety-ways-to-stay-grounded.php"> mindfulness for anxiety ways to stay grounded ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+ 
+
+<?php include('../include/footer.php'); ?>
---
+++ fiverr/php/michiganpsychologicalcare-blogs/2026/02/php/mindfulness-for-anxiety-ways-to-stay-grounded.php
@@ -0,0 +1,97 @@
+<?php 
+
+$img = '';
+$title = "Mindfulness for Anxiety: Ways to Stay Grounded";
+$meta_desc = '';
+
+?>
+<?php include('../include/header.php'); ?>
+
+		<!-- Page Title
+		============================================= -->
+<section id="content">
+			<div class="content-wrap">
+				<div class="container clearfix">
+
+					<div class="single-post mb-0">
+
+						<!-- Single Post
+						============================================= -->
+						<div class="entry clearfix">
+
+							<!-- Entry Title
+							============================================= -->
+							<div class="entry-title">
+								<h2>Mindfulness for Anxiety: Ways to Stay Grounded</h2>
+							</div><!-- .entry-title end -->
+
+							<!-- Entry Meta
+							============================================= -->
+							<div class="entry-meta">
+								<ul>
+									<li><i class="icon-calendar3"></i>Feb 10, 2026</li>
+								</ul>
+							</div><!-- .entry-meta end -->
+
+							<!-- Entry Image
+							============================================= -->
+							<div class="entry-image bottommargin">
+								<a href="#"><img src="/images/blog/2026/02/mindfulness-for-anxiety-ways-to-stay-grounded.jpg" alt="Blog Single"></a>
+							</div><!-- .entry-image end -->
+
+							<!-- Entry Content
+							============================================= -->
+							<div class="entry-content mt-0">
+                                
+                                <h1><strong>Mindfulness for Anxiety: Ways to Stay Grounded</strong></h1>
+                                <p>Anxiety is an ever-present force that affects people all over the world. When people experience anxiety to varying degrees, they may need to seek treatment. Anxiety treatment can include medication, therapy, support groups, and coping skills. One popular coping skill that is used to manage anxiety is mindfulness. <a href="https://www.sciencedirect.com/science/article/abs/pii/S1747938X15000676">Mindfulness</a> is the practice of staying present in the moment while staying aware of thoughts, emotions, and physical changes. One of the most important aspects of mindfulness is maintaining a non-judgmental approach towards our thoughts and emotions.&nbsp;</p>
+                                <h2><strong>How Is Mindfulness Effective for Anxiety Relief</strong></h2>
+                                <p>Mindfulness for <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC3679190/">anxiety relief</a> helps interrupt rumination and anxious thoughts that are typical of anxiety. Practicing mindfulness helps people with anxiety shift their bodies from fight or flight to a more regulated state. Mindfulness can be practiced at any point throughout a person&rsquo;s day. However, if people are looking for a more structured way to practice mindfulness, some common techniques can help.</p>
+                                <h2><strong>Ways to Practice Mindfulness</strong></h2>
+                                <p>As we mentioned previously, mindfulness can be practiced anywhere. However, there are some techniques that people use to help incorporate mindfulness into their days.</p>
+                                <ul>
+                                    <li><em>Breathing Techniques</em>: Breathing techniques can be used to help calm the nervous system by slowing breathing through deliberate breaths. An example of a breathing technique is inhaling for 4 seconds and exhaling for 6 seconds. <br /><br /></li>
+                                    <li><em>Grounding Techniques</em>: Grounding techniques can be used to help decrease anxiety or anxious spirals. An example of a grounding technique is counting 5 things you can see, 4 things you can feel, 3 things you can hear, 2 things you can smell, and 1 thing you can taste. This technique helps interrupt anxious spirals and bring your presence back to the moment. <br /><br /></li>
+                                    <li><em>Mindfulness Meditations</em>: <a href="https://www.apa.org/topics/mindfulness/meditation">Mindfulness meditations</a> are structured practices that teach focusing on the present moment through meditation. Meditations can be structured or guided, or they can be created by the person practicing.&nbsp;</li>
+                                </ul>
+                                <p>Stress and anxiety can be very powerful if left untreated or dealt with. <a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC3336928/">Stress reduction</a> is a common way that people can help prevent anxiety from getting worse. Mindfulness is a learned skill that people can use to help decrease stress levels in their lives. Mindfulness teaches us how to stay present in the moment without judgment. Staying present in the moment can help reduce anxious thoughts and physical sensations that come with anxiety. People can practice mindfulness through breathing techniques, grounding techniques, and mindfulness meditations. Mindfulness can help provide anxiety relief for people experiencing stress. While mindfulness can take some time to learn and practice, it&rsquo;s always worth the effort for people who are willing to give it a try. Practicing mindfulness can help to improve anxiety levels and mental health as a whole.</p>
+                                <p>If you or someone you love is interested in working with us, we are here for you! <a href="https://michiganpsychologicalcare.com/contact.php">Reach out to us</a> today if you are ready to start.</p>
+                                <p><strong>References</strong></p>
+                                <p><a href="https://www.sciencedirect.com/science/article/abs/pii/S1747938X15000676">https://www.sciencedirect.com/science/article/abs/pii/S1747938X15000676</a>&nbsp;</p>
+                                <p><a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC3679190/">https://pmc.ncbi.nlm.nih.gov/articles/PMC3679190/</a></p>
+                                <p><a href="https://www.apa.org/topics/mindfulness/meditation">https://www.apa.org/topics/mindfulness/meditation</a>&nbsp;</p>
+                                <p><a href="https://pmc.ncbi.nlm.nih.gov/articles/PMC3336928/">https://pmc.ncbi.nlm.nih.gov/articles/PMC3336928/</a>&nbsp;</p>
+                                <p><strong>Keywords</strong>: mindfulness, anxiety relief, mindfulness meditation, stress reduction</p>
+                                
+                                <!-- Post Single - Content End -->
+
+								<div class="clear"></div>
+
+								<!-- Post Single - Share
+								============================================= -->
+                        		<?php
+                        		include('../include/share_button.php');
+                        		?>
+                        		<!-- Post Single - Share End -->
+							</div>
+						</div><!-- .entry end -->
+
+							<!-- Post Navigation
+							============================================= -->
+							<div class="row justify-content-between col-mb-30 post-navigation">
+							<div class="col-12 col-md-auto text-center">
+								<a href="/blog/how-to-recognize-and-reframe-catastrophic-thinking.php">⇐ how to recognize and reframe catastrophic thinking </a>
+							</div>
+
+							<div class="col-12 col-md-auto text-center">
+								<a  href="/blog/stress-management-strategies-for-highly-sensitive-people.php"> stress management strategies for highly sensitive people ⇒</a>
+							</div>
+						</div><!-- .post-navigation end -->
+						<div class="line"></div>
+					</div>
+				</div>
+			</div>
+		</section><!-- #content end -->
+ 
+
+<?php include('../include/footer.php'); ?>
